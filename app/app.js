91effa9778062f86102a24157fc3bfa43747630e
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-06-12 12:31:33 +0900

    Fix Express-route

---
+++ app/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 3000;
+
+app.get('/api/v1/', function (req, res){
+    res.json(
+        {
+            message: "hello"
+        }
+    );
+});
+
+app.listen(port);
+console.log('PORT:' + port);
