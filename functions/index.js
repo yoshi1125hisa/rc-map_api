1beee95696214b110890a62e56137ffc551224ee
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-06-27 21:40:50 +0900

    Update firebase-setting

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,17 @@
 const functions = require('firebase-functions');
-const express = require('express');
+var admin = require("firebase-admin");
 
+const serviceAccount = require("./railroadcrossing-1555889111567-firebase-adminsdk-4nx2c-dc0208f00e.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://railroadcrossing-1555889111567.firebaseio.com"
+});
+
+const db = admin.database();
+const ref = db.ref("data");
+
+const express = require('express');
 const app = express();
 
 
@@ -11,7 +22,16 @@ exports.app = functions.https.onRequest((req, res) => {
   });
 
   app.get('/all', (request, response) => {
-    response.send("This is All endpoint");
+    // response.send("This is All endpoint");
+    /*　room1以下に対しての非同期コールバック */
+    ref.on("value", function (snapshot) {
+        /* ここに取得したデータを用いた何らかの処理 */
+        response.send(snapshot.val());
+        console.log(snapshot.val());
+      },
+      function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      });
   });
 
   app.get('/prefecture', (request, response) => {
